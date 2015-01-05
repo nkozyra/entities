adba9049b35d24b7bec54c8e8a76db4e66f9cb59
Nathan Kozyra <nkozyra@gmail.com>
2015-01-05 15:25:42 -0500

    Address package skeleton

---
+++ addresses/addresses.go
@@ -0,0 +1,21 @@
+//	Address tokenizer and normalization package
+//	https://github.com/nkozyra/entities
+
+package addresses
+
+import
+(
+)
+
+type Address struct {
+	Raw string
+	Normalized string
+}
+
+func (a *Address) Init() {
+
+}
+
+func Normalize() {
+
+}
